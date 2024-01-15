6e043d11760659f1dfe24ab05d1538476ac63a97
yano-runteq <mayfirrmiana@au.com>
2024-01-16 08:54:59 +0900

    add: デフォルトのセクションを追加 clean: のコードを整理

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,8 +1,6 @@
 class TopsController < ApplicationController
   def top
     # ログイン機能実装時に書き換え予定
-    user = User.find_by(student_identification: "test")
-    timetable = user.timetable
-    @sections = timetable.sections
+    @sections = default_sections
   end
 end
