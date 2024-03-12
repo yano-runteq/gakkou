a39c2f1d9232c5ee6f21dd7c3c78aa764b747d92
yano-runteq <mayfirrmiana@au.com>
2024-03-12 14:49:18 +0900

    add: contactページのMVC・ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root "tops#top"
+
+  resources :contacts, only: %i[new create] do
+    collection do
+      get "contacts/check", to: "contacts#check"
+      get "contacts/completed", to: "contacts#completed"
+    end
+  end
 end
